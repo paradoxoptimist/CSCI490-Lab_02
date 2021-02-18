cf51fdfe618c3a6d664e04131ed2a5464dfe9e6a
Emily Haines <haineseb@g.cofc.edu>
2021-02-18 18:34:36 -0500

    edited activityMain xml

--- app/src/main/java/com/example/lab2/SecondActivity.java
+++ app/src/main/java/com/example/lab2/SecondActivity.java
@@ -1,4 +1,34 @@
 package com.example.lab2;
 
-public class SecondActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent i = getIntent();
+        String s = i.getExtras().getString("testString");
+
+        textView2 = findViewById(R.id.textView2);
+
+        textView2.setText(s);
+
+        finish();
+    }
+
+    @Override
+    public void finish(){
+        Intent intent = new Intent();
+
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
 }
